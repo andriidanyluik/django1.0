a7dd548be4b96ac82bc11a7cd1a8241f66f4de46
andrii <andriidanyluik@gmail.com>
2020-04-17 20:43:28 +0300

    5th admin is en

--- site1/site1/apps/articles/models.py
+++ site1/site1/apps/articles/models.py
@@ -1,10 +1,23 @@
+import datetime
 from django.db import models
+
+from django.utils import timezone
 
 class Article(models.Model):
 	article_title = models.CharField("nazva statti", max_length = 200)
 	article_text = models.TextField("text statti" )
-	pub_date = models.DateTimeField()
-class Comment(models.Model):
-	author
+	pub_date = models.DateTimeField('data publikacii')
 
+	def __str__(self):
+		return self.article_title
+	def was_published_recently(self):
+		return self.pub_date >= (timezone.now() - datetime.timedelta(days = 7))
+
+class Comment(models.Model):
+	article = models.ForeignKey(Article, on_delete = models.CASCADE)
+	author_name = models.CharField("imya avtor", max_length = 50)
+	comment_text = models.CharField('text comment', max_length = 200)
+
+	def __str__(self):
+		return self.author_name
 
