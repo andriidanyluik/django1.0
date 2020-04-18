17669545566bc2905335c09dba32b24d02090cc5
andrii <andriidanyluik@gmail.com>
2020-04-18 23:18:42 +0300

    11 stattya on web 100%

--- site1/site1/apps/articles/views.py
+++ site1/site1/apps/articles/views.py
@@ -1,3 +1,4 @@
+from django.http import Http404, HttpResponseRedirect
 from django.shortcuts import render
 
 from .models import Article, Comment
@@ -7,4 +8,9 @@ def index(request):
 	return render(request, 'articles/list.html', {'latest_articles_list': latest_articles_list})
 
 def detail(request, article_id):
-	pass
+	try:
+		a = Article.objects.get( id = article_id )
+	except:
+		raise Http404("Статтю не знайдено!")
+
+	return render(request, 'articles/detail.html', {'article': a})
