f609372453da64d89b6b8d13e941929149a2f675
andrii <andriidanyluik@gmail.com>
2020-04-19 00:05:59 +0300

    12 comment add and web!

--- site1/site1/apps/articles/views.py
+++ site1/site1/apps/articles/views.py
@@ -1,5 +1,6 @@
 from django.http import Http404, HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
 from .models import Article, Comment
 
@@ -13,4 +14,15 @@ def detail(request, article_id):
 	except:
 		raise Http404("Статтю не знайдено!")
 
-	return render(request, 'articles/detail.html', {'article': a})
+	latest_comment_list = a.comment_set.order_by('-id')[:10]
+	return render(request, 'articles/detail.html', {'article': a, 'latest_comment_list': latest_comment_list})
+
+def leave_comment(request, article_id):
+	try:
+		a = Article.objects.get( id = article_id )
+	except:
+		raise Http404("Статтю не знайдено!")
+
+	a.comment_set.create(author_name = request.POST['name'], comment_text = request.POST['text'])
+
+	return HttpResponseRedirect( reverse('articles:detail', args = (a.id,)) )
